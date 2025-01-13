60cda48b96489741f30ac8eda0530044544f4da0
gustof chatami <eov@proton.me>
2025-01-13 23:28:23 +0700

    brick by brick

--- init.lua
+++ init.lua
@@ -1,6 +1,24 @@
-if not pcall(require, "impatient") then
-  print "impatient.nvim tidak ditemukan!"
-end
- 
-require('plugins')
-require('settings') 
+-- BASIC
+vim.opt.number = true --menampilkan nomor baris
+-- vim.opt.relativenumber = true -- menampilkan baris relatif
+vim.opt.wrap = false
+vim.opt.expandtab = true -- use space for tab
+vim.opt.tabstop = 4 -- lebar tab 4 spasi
+vim.opt.shiftwidth = 4 -- lebar indent 4 spasi
+vim.opt.smartindent = true
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- looks
+vim.opt.termguicolors = true
+vim.cmd[[colorscheme desert]]
+
+--remap
+vim.g.mapleader = " "
+vim.keymap.set('i', 'nn', '<ESC>')
+vim.keymap.set('n', 'n', 'h')  -- kiri
+vim.keymap.set('n', 'o', 'j')  -- bawah
+vim.keymap.set('n', 'i', 'k')  -- atas
+vim.keymap.set('n', 'e', 'l')  -- kanan
